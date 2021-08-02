08748034d844c5c65c5ec276033a11d5df75883f
jirayus013t <johnny.sithi@gmail.com>
2021-08-02 14:58:53 +0700

    first time commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+  
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'ap-southeast-1'});
+
+// Create the DynamoDB service object
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+// Call DynamoDB to retrieve the list of tables
+ddb.listTables({Limit: 100}, function(err, data) {
+  if (err) {
+    console.log("Error", err.code);
+  } else {
+    console.log("Table names are ", data.TableNames);
+  }
+});
